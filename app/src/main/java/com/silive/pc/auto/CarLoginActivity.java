b58bfb2cb7c5cfb3acc7e53b08f935a7ba4668be
jadaungeetanjali <jadaungeetanjali@gmail.com>
2017-11-29 00:56:13 +0530

    first commit

---
+++ app/src/main/java/com/silive/pc/auto/CarLoginActivity.java
@@ -0,0 +1,13 @@
+package com.silive.pc.auto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CarLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_login);
+    }
+}
